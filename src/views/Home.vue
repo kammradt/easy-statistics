5ba6ec5f7678c341b26bfc39ed7b2406787713d5
Kammradt <vinicius.kammradt1@gmail.com>
2020-03-09 19:10:44 -0300

    :art: Better speed-dial floating buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,15 @@
     <v-toolbar color="#F6F6F6">
       <v-toolbar-title class="font-weight-light" v-text="$t('title')"/>
       <v-spacer/>
-      <v-speed-dial direction="bottom">
+      <v-speed-dial direction="bottom" absolute right>
         <template v-slot:activator>
           <v-btn icon large>
             <v-icon v-text="'mdi-earth'"/>
           </v-btn>
         </template>
-        <v-btn fab v-for="language in languages" :key="language.lang" v-text="language.emoji"
-               @click="setLanguageTo(language.lang)" class="display-1"/>
-        <v-btn fab link target="_blank" href="https://github.com/kammradt/easy-statistics">
+        <v-btn v-for="language in languages" :key="language.lang" v-text="language.emoji"
+               small rounded class="title"  @click="setLanguageTo(language.lang)"/>
+        <v-btn small rounded class="title" link target="_blank" href="https://github.com/kammradt/easy-statistics">
           <v-icon v-text="'mdi-xml'"/>
         </v-btn>
       </v-speed-dial>
