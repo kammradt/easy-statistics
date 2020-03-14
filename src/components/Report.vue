f35338461642398377cb76896c1ef9a4a00d2b36
Kammradt <vinicius.kammradt1@gmail.com>
2020-03-14 10:20:32 -0300

    :sparkles: Adding a main Report component

---
+++ src/components/Report.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <v-text-field
+            class="my-6 font-weight-light title"
+            :label="$t('input.label')"
+            v-model="numbers" clearable
+            @click:clear="onErase"
+    />
+
+    <v-card tile>
+      <div class="text-center pt-3">
+        <span class="display-1 font-weight-light" v-text="$t('card.title')"/> <br>
+        <span class="subtitle-2 grey--text pt-1" v-text="$t('card.subtitle')"/>
+      </div>
+      <v-card-text>
+        <v-row class="pb-3">
+          <v-col @click="copy(ordered)" cols="12">
+            <v-btn block tile outlined v-text="$t(`report.ordered`)"/>
+            <v-card @click="({})" v-text="ordered" outlined tile class="text-wrap text-center"/>
+          </v-col>
+
+          <v-col v-for="(result, operation) in report" :key="operation" @click="copy(result)">
+            <v-btn block tile outlined v-text="$t(`report.${operation}`)"/>
+            <v-btn block tile outlined text v-text="result"/>
+          </v-col>
+
+          <v-col cols="12" class="mt-3">
+            <div class="text-center pt-3 pb-3">
+              <span class="display-1 font-weight-light" v-text="$t('card.frequency.title')"/> <br>
+              <span class="subtitle-2 grey--text pt-1" v-text="$t('card.frequency.subtitle')"/>
+            </div>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left" v-text="$t('card.frequency.number')"/>
+                  <th class="text-left" v-text="$t('card.frequency.absolute')"/>
+                  <th class="text-left" v-text="$t('card.frequency.relative')"/>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(frequency, number) in frequencyReport" :key="number">
+                  <td v-text="number">
+                  <td :key="type" v-for="(value, type) in frequency"
+                      v-text="getFormattedFrequency(value, type)" @click="copy(value)"/>
+                </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-col>
+        </v-row>
+
+        <CardFooter/>
+
+      </v-card-text>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+  import {filter, generateReport, getFrequencyReport, order} from "../services/statistics";
+  import CardFooter from "./CardFooter";
+
+  export default {
+    name: "Report",
+    components: {
+      CardFooter
+    },
+    data: () => ({
+      numbers: '1, 2, 3'
+    }),
+    created() {
+      this.numbers = '10, 20, 50, 30, 30, 30'
+      document.title = this.$t('title')
+    },
+    methods: {
+      copy(value) {
+        navigator.clipboard.writeText(value)
+      },
+      getFormattedFrequency(value, type) {
+        let formats = {
+          relative_frequency: '%',
+          absolute_frequency: this.$t('card.frequency.absolute_description')
+        }
+        return `${value} ${formats[type]}`
+      },
+      onErase() {
+        this.numbers = '1, 2, 3'
+        setTimeout(() => this.numbers = '', 50)
+      },
+    },
+    computed: {
+      listOfNumbers() {
+        return filter(this.numbers)
+      },
+      report() {
+        return generateReport(this.listOfNumbers);
+      },
+      frequencyReport() {
+        return getFrequencyReport(this.listOfNumbers)
+      },
+      ordered() {
+        return order(this.listOfNumbers)
+      }
+    }
+  }
+</script>
